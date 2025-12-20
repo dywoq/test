9e8e218292ad477d5b958ac21270921c06d13a0d
dywoq <aleks.koyf@gmail.com>
2025-12-20 10:17:35 +0300

    feature: Add info.hxx

---
+++ include/dywoq/test/info.hxx
@@ -0,0 +1,20 @@
+#ifndef _DYWOQ_TEST_FUNCTION_HXX
+#define _DYWOQ_TEST_FUNCTION_HXX
+
+#include <cstdint>
+#ifdef __cplusplus
+#if __cplusplus >= 202002LL
+
+namespace dywoq::test {
+  // Represents the information collected during running tests
+  // and available after running them.
+  struct info {
+    bool has_failures = false;
+    std::uint32_t failures_count = 0;
+  };
+} // namespace dywoq::test
+
+#endif
+#endif
+
+#endif
